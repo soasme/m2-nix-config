95e4d4cad6e788d349e77460fc957ad78f4a9c2e
soasme <soasme@gmail.com>
2024-09-04 22:41:56 +1200

    allow unfree.

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export NIXPKGS_ALLOW_UNFREE=1
+
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
