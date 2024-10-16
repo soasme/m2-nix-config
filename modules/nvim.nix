de8ed86d1ae92abac67402f4e7b895488e1f92f0
soasme <soasme@gmail.com>
2024-10-17 08:07:40 +1300

    install leap and set avante config.

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,5 +1,4 @@
 { pkgs }:
-
 {
   enable = true;
   viAlias = true;
@@ -16,9 +15,11 @@
     lazy-nvim
   ];
   extraConfig = ''
-    set relativenumber
-
     lua <<EOF
+
+    -- set rno
+    vim.wo.relativenumber = true
+
     -- Bootstrap lazy.nvim
     local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
     if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -51,6 +52,12 @@
         { "dracula/vim", name = "dracula" },
         { "LnL7/vim-nix" },
         {
+          "ggandor/leap.nvim",
+          config = function()
+            require('leap').create_default_mappings()
+          end,
+        },
+        {
           "sbdchd/neoformat",
           config = function()
             vim.g.neoformat_enabled_nix = {"nixfmt"}
@@ -62,7 +69,91 @@
           event = "VeryLazy",
           lazy = false,
           version = false,
-          opts = {},
+          opts = {
+            provider = "claude", -- Recommend using Claude
+            auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+            claude = {
+              endpoint = "https://api.anthropic.com",
+              model = "claude-3-5-sonnet-20240620",
+              temperature = 0,
+              max_tokens = 4096,
+            },
+            behaviour = {
+              auto_suggestions = true, -- Experimental stage
+              auto_set_highlight_group = true,
+              auto_set_keymaps = true,
+              auto_apply_diff_after_generation = false,
+              support_paste_from_clipboard = false,
+            },
+            mappings = {
+              --- @class AvanteConflictMappings
+              diff = {
+                ours = "co",
+                theirs = "ct",
+                all_theirs = "ca",
+                both = "cb",
+                cursor = "cc",
+                next = "]x",
+                prev = "[x",
+              },
+              suggestion = {
+                accept = "<M-l>",
+                next = "<M-]>",
+                prev = "<M-[>",
+                dismiss = "<C-]>",
+              },
+              jump = {
+                next = "]]",
+                prev = "[[",
+              },
+              submit = {
+                normal = "<CR>",
+                insert = "<C-s>",
+              },
+              sidebar = {
+                apply_all = "A",
+                apply_cursor = "a",
+                switch_windows = "<Tab>",
+                reverse_switch_windows = "<S-Tab>",
+              },
+            },
+            hints = { enabled = true },
+            windows = {
+              ---@type "right" | "left" | "top" | "bottom"
+              position = "right", -- the position of the sidebar
+              wrap = true, -- similar to vim.o.wrap
+              width = 30, -- default % based on available width
+              sidebar_header = {
+                align = "center", -- left, center, right for title
+                rounded = true,
+              },
+              input = {
+                prefix = "> ",
+              },
+              edit = {
+                border = "rounded",
+                start_insert = true, -- Start insert mode when opening the edit window
+              },
+              ask = {
+                floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+                start_insert = true, -- Start insert mode when opening the ask window, only effective if floating = true.
+                border = "rounded",
+              },
+            },
+            highlights = {
+              ---@type AvanteConflictHighlights
+              diff = {
+                current = "DiffText",
+                incoming = "DiffAdd",
+              },
+            },
+            --- @class AvanteConflictUserConfig
+            diff = {
+              autojump = true,
+              ---@type string | fun(): any
+              list_opener = "copen",
+            },
+          },
           build = "make",
           dependencies = {
             "nvim-treesitter/nvim-treesitter",
